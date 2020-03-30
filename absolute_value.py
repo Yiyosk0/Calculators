d430545c6b97d175f86dbacecf12e5f0549c86ad
Rodrigo Martinez <rodrigomartinezbecerra@gmail.com>
2020-03-30 17:14:49 -0300

    First commit

---
+++ absolute_value.py
@@ -0,0 +1,16 @@
+import math
+
+def absolutev ():
+    first = int(input("Type the first number: "))
+    second = int(input("Type the second number: "))
+    result = math.hypot(first, second)
+    print("Result: " + str(result) + "\n")
+
+while(True):
+    absolutev()
+
+
+#Made in 3/30/2020 
+
+
+
